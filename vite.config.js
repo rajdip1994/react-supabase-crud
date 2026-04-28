41f0349e4bc9ec3f8d0886f108d57a454d51b276
joydip ghosal <joydip.ghosal@gmail.com>
2026-04-28 19:16:17 +0530

    Added PWA option

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,36 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: false
+      },
+      manifest: {
+        name: 'User Management App',
+        short_name: 'UserApp',
+        description: 'CRUD App with Supabase',
+        theme_color: '#0d6efd',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/launchericon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/launchericon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
